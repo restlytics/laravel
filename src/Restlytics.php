8797afc56696466db27a20f4751ff525a26c1561
Restlytics <35517017+restlytics@users.noreply.github.com>
2018-01-22 11:20:22 -0800

    Update Restlytics.php

--- src/Restlytics.php
+++ src/Restlytics.php
@@ -1,10 +1,9 @@
 <?php namespace Restlytics;
 
-use Illuminate\Support\Facades\Route;
-use GuzzleHttp\Client;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
 
 class Restlytics
 {
@@ -13,41 +12,40 @@ class Restlytics
 		$response = $next($request);
 		
 		$currentRoute = Route::current();
-		if(!$currentRoute)
+		if (!$currentRoute)
 		{
 			return $response;
 		}
 		
 		$method = request()->method();
-		if($method == 'OPTIONS')
+		if ($method == 'OPTIONS')
 		{
 			return $response;
 		}
 		
 		$apiKey = config('restlytics.api_key');
 		$apiSecret = config('restlytics.api_secret');
-		
 		if (empty($apiKey) || empty($apiSecret))
 		{
 			return $response;
 		}
 		
-        $endpoint = $currentRoute->uri();
-        if (in_array($endpoint, config('restlytics.ignore_endpoints', [])))
-        {
-            return $response;
-        }
-
-        $headers = array_filter($request->headers->all(), function($key)
-        {
-	        return !in_array($key, config('restlytics.ignore_headers'));
-        }, ARRAY_FILTER_USE_KEY);
+		$endpoint = $currentRoute->uri();
+		if (in_array($endpoint, config('restlytics.ignore_endpoints', [])))
+		{
+			return $response;
+		}
 		
-		$body = array_filter($request->all(), function($key)
+		$headers = array_filter($request->headers->all(), function ($key)
+		{
+			return !in_array($key, config('restlytics.ignore_headers'));
+		}, ARRAY_FILTER_USE_KEY);
+		
+		$body = array_filter($request->all(), function ($key)
 		{
 			return !in_array($key, config('restlytics.ignore_request_params'));
 		}, ARRAY_FILTER_USE_KEY);
-
+		
 		
 		$params = [
 			'method' => $method,
@@ -56,23 +54,16 @@ class Restlytics
 			'response_code' => $response->status(),
 			'ip_address' => $request->ip(),
 			'request_headers' => $headers,
-            'request_body' => $body,
+			'request_body' => $body,
 			'user_agent' => $request->userAgent(),
 			'requested_at' => $_SERVER['REQUEST_TIME_FLOAT'],
 			'time_taken' => microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
 		];
 		
 		Log::info(json_encode($params));
-
-		$client = new Client();
-		$client->postAsync("http://api.restlytics.com/track", [
-			'body' => json_encode($params),
-			'auth' => [
-                $apiKey,
-                $apiSecret
-			]
-		]);
-
+		
+		dispatch(new RestlyticsJob($apiKey, $apiSecret, $params));
+		
 		return $response;
 	}
 }
